348d8d4180e620095a65eaf9ba1f8b1ea8a664fa
overflowz <segmfaultz@gmail.com>
2025-06-01 23:15:07 +0400

    disable inlay hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -50,14 +50,15 @@ return {
           },
           settings = {
             typescript = {
-              inlayHints = {
-                parameterNames = { enabled = "literals" },
-                parameterTypes = { enabled = true },
-                variableTypes = { enabled = true },
-                propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                enumMemberValues = { enabled = true },
-              },
+              inlayHints = nil,
+              -- inlayHints = {
+              --   parameterNames = { enabled = "literals" },
+              --   parameterTypes = { enabled = true },
+              --   variableTypes = { enabled = true },
+              --   propertyDeclarationTypes = { enabled = true },
+              --   functionLikeReturnTypes = { enabled = true },
+              --   enumMemberValues = { enabled = true },
+              -- },
             },
           },
         },
